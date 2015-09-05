a5dd3f45e6e957ab494de486bef8e810c34cf981
Felix Lau <felixlaumon@gmail.com>
2015-09-06 00:56:02 +0800

    Add ubuntu script

---
+++ ubuntu-ppa.sh
@@ -0,0 +1,5 @@
+# PPA
+sudo add-apt-repository -y ppa:pi-rho/dev  # tmux
+sudo add-apt-repository ppa:neovim-ppa/unstable  # neovim
+sudo add-apt-repository ppa:saiarcot895/myppa  # apt-fast
+sudo apt-get update
