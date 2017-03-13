cc1c8bd1f43be9fa57bb56094446882b9ce65b96
Felix Lau <felixlaumon@gmail.com>
2017-03-13 09:45:02 -0700

    Updates

---
+++ tmux-layouts/default-4up.window.sh
@@ -0,0 +1,12 @@
+window_root "~"
+new_window "default-4up"
+
+split_h 50
+
+select_pane 1
+split_v 50
+
+select_pane 3
+split_v 50
+
+select_pane 1
--- tmux-layouts/monitor.window.sh
+++ tmux-layouts/monitor.window.sh
@@ -6,17 +6,11 @@ split_h 35
 select_pane 2
 split_v 50
 
-select_pane 2
-split_v 50
-
-select_pane 4
-split_v 50
-
 select_pane 1
-run_cmd "glances -t 5"
-select_pane 4
+run_cmd "glances -b -t 5"
+select_pane 2
 run_cmd "watch_gpu_util"
-select_pane 5
+select_pane 3
 run_cmd "watch_gpu"
 
 select_pane 1
