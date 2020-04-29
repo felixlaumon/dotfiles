dcb30768de305f5c89e0dc3d7a17f32e07ba037a
Felix Lau <felixlaumon@gmail.com>
2020-04-29 01:47:42 -0700

    Add nibar

---
+++ nibar/lib/styles.jsx
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    fg: "rgba(255,255,255,0.75)",
+    dim: "rgba(255,255,255,0.5)",
+    bg: "#000",
+    red: "#ff8700",
+    accent: "#5fafaf"
+  },
+  fontSize: "10px",
+  lineHeight: "21px",
+  fontWeight: "Regular",
+  fontFamily: "'Iosevka Term', 'SF Mono', monospace"
+}
