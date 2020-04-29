cfa4e6f2eec4328e3b914c148446c626fc81af1a
Felix Lau <felixlaumon@gmail.com>
2020-04-29 01:49:09 -0700

    Remove old files

--- ubuntu-ppa.sh
+++
@@ -1,5 +0,0 @@
-# PPA
-sudo add-apt-repository -y ppa:pi-rho/dev  # tmux
-sudo add-apt-repository ppa:neovim-ppa/unstable  # neovim
-sudo add-apt-repository ppa:saiarcot895/myppa  # apt-fast
-sudo apt-get update
