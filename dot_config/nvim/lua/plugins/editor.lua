4c857219449f0cc9433e3521115818cd9cea0b96
Felix Lau <felixlaumon@gmail.com>
2023-05-06 00:29:42 -0700

    Update

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -139,17 +139,28 @@ return {
   {
     "RRethy/vim-illuminate",
     event = { "BufReadPost", "BufNewFile" },
-    opts = { delay = 100 },
+    opts = { delay = 100, filetypes_denylist = { "telescope", "trouble" } },
     config = function(_, opts)
       require("illuminate").configure(opts)
       local function map(key, dir, buffer)
-        vim.keymap.set("n", key, function()
-          require("illuminate")["goto_" .. dir .. "_reference"](false)
-        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+        vim.keymap.set(
+          "n",
+          key,
+          function() require("illuminate")["goto_" .. dir .. "_reference"](true) end,
+          { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer }
+        )
       end
 
       map("]]", "next")
       map("[[", "prev")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
     end,
     keys = {
       { "]]", desc = "Next Reference" },
