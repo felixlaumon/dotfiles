4e7540bca5223bbea65c5a26c60264355e350a4d
Felix Lau <felixlaumon@gmail.com>
2023-09-29 13:41:24 -0700

    Update

--- dot_config/nvim/lua/plugins/magma.lua
+++ dot_config/nvim/lua/plugins/magma.lua
@@ -3,6 +3,9 @@ return {
     "dccsillag/magma-nvim",
     version = "*",
     build = ":UpdateRemotePlugins",
+    opts = function ()
+      vim.g.magma_image_provider = "kitty"
+    end,
     keys = {
       {
         "<leader>mi",
@@ -11,7 +14,7 @@ return {
       },
       { "<leader>mo", "<cmd>MagmaEvaluateOperator<CR>", desc = "Evaluate text given by some operator" },
       { "<leader>ml", "<cmd>MagmaEvaluateLine<CR>",     desc = "Evaluate current line" },
-      { "<leader>mv", "<cmd>MagmaEvaluateVisual<CR>",   desc = "Evaluate selected text",                mode = { "v" } },
+      { "<leader>mv", "<cmd>MagmaEvaluateVisual<CR>",   desc = "Evaluate selected text",                mode = { "v" }, slient = true },
       { "<leader>mc", "<cmd>MagmaEvaluateOperator<CR>", desc = "Reevaluate the currently selected cell" },
       { "<leader>mr", "<cmd>MagmaRestart!<CR>",         desc = "Restarts the current kernel" },
       { "<leader>mx", "<cmd>MagmaInterrupt<CR>",        desc = "Interrupts the currently running cell" },
