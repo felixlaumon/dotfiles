1577c22cef1c7d985471322c5fe1ce75c64f5b3e
Felix Lau <felixlaumon@gmail.com>
2023-10-06 15:12:51 -0700

    Update

--- dot_config/nvim/lua/plugins/magma.lua
+++ dot_config/nvim/lua/plugins/magma.lua
@@ -1,9 +1,17 @@
 return {
   {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>m"] = { name = "+magma" },
+      },
+    },
+  },
+  {
     "dccsillag/magma-nvim",
     version = "*",
     build = ":UpdateRemotePlugins",
-    opts = function ()
+    opts = function()
       vim.g.magma_image_provider = "kitty"
     end,
     keys = {
@@ -14,7 +22,13 @@ return {
       },
       { "<leader>mo", "<cmd>MagmaEvaluateOperator<CR>", desc = "Evaluate text given by some operator" },
       { "<leader>ml", "<cmd>MagmaEvaluateLine<CR>",     desc = "Evaluate current line" },
-      { "<leader>mv", "<cmd>MagmaEvaluateVisual<CR>",   desc = "Evaluate selected text",                mode = { "v" }, slient = true },
+      {
+        "<leader>mv",
+        "<cmd>MagmaEvaluateVisual<CR>",
+        desc = "Evaluate selected text",
+        mode = { "v" },
+        slient = true
+      },
       { "<leader>mc", "<cmd>MagmaEvaluateOperator<CR>", desc = "Reevaluate the currently selected cell" },
       { "<leader>mr", "<cmd>MagmaRestart!<CR>",         desc = "Restarts the current kernel" },
       { "<leader>mx", "<cmd>MagmaInterrupt<CR>",        desc = "Interrupts the currently running cell" },
