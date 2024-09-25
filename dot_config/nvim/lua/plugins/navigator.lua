4e6e1238dcc4b561882c4d4cd0f8e876ff44040c
Felix Lau <felixlaumon@gmail.com>
2024-09-24 23:40:04 -0700

    Updates

---
+++ dot_config/nvim/lua/plugins/navigator.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "numToStr/Navigator.nvim",
+    config = function()
+      require("Navigator").setup()
+    end,
+    keys = {
+      { "<c-h>", "<cmd>NavigatorLeft<cr>", silent = true, desc = "Left pane" },
+      { "<c-j>", "<cmd>NavigatorDown<cr>", silent = true, desc = "Down pane" },
+      { "<c-k>", "<cmd>NavigatorUp<cr>", silent = true, desc = "Up pane" },
+      { "<c-l>", "<cmd>NavigatorRight<cr>", silent = true, desc = "Right pane" },
+    },
+  },
+}
